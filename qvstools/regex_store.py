4352285485b4004bd08786fbf59518c11bbb13a4
Ben Simonds <BSSimonds@gmail.com>
2016-04-07 14:10:05 +0100

    Update regex_store.py

--- qvstools/regex_store.py
+++ qvstools/regex_store.py
@@ -1,16 +1,11 @@
 """Here we just store some shared regex searches"""
 import re
 
-search_strings = {
+searches = {
     #First functional version of a file finder for log files.
-    'filesearchstring1':r"(?:\s|\[|^)((?:\.\.|\\\\|\\)*[\\\w \-_]+)\.(qvd|xls[xm]?)\]?",
+    'filesearchstring1':re.compile(r"(?:\s|\[|^)((?:\.\.|\\\\|\\)*[\\\w \-_]+)\.(qvd|xls[xm]?)\]?"),
     #Second attempt, differentiates between "[strings with spaces]" and "stringswithoutspaces"
-    'filesearchstring2':r"(?:\s|^)\[((?:\.\.|\\\\|\\)*[\\\w \-_]+)\.(qvd|xls[xm]?|csv|qvx)\]|(?:\s|^)((?:\.\.|\\\\|\\)*[\\\w\-_]+)\.(qvd|xls[xm]?|csv|qvx)"
+    'filesearchstring2':re.compile(r"(?:\s|^)\[((?:\.\.|\\\\|\\)*[\\\w \-_]+)\.(qvd|xls[xm]?|csv|qvx)\]|(?:\s|^)((?:\.\.|\\\\|\\)*[\\\w\-_]+)\.(qvd|xls[xm]?|csv|qvx)"),
     #Store search string, matches store x into y statements.
-    'storesearchstring':r"store\s\[?[\w\s]*\]?\sinto"
+    'storesearchstring':re.compile(r"store\s\[?[\w\s]*\]?\sinto")
 }
-
-#Compile search strings.
-searches = {}
-for s in search_strings:
-    searches[s] = re.compile(search_strings[s])
