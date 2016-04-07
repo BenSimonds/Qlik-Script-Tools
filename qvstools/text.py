26f816302b019e359ccb229fcdffd418af23822f
Ben Simonds <BSSimonds@gmail.com>
2016-04-07 13:37:25 +0100

    Work on text module.

--- qvstools/text.py
+++ qvstools/text.py
@@ -32,6 +32,12 @@ def detect_encoding(textfile):
 				pass
 	return None
 
+def decode_with_detected(textfile):
+	"""Uses detect_encoding to get encoding and returns a unicode string of the text file"""
+	encoding  = detect_encoding(textfile)
+	with open(textfile,'r',encoding=encoding,errors='replace') as f:
+		fulltext = f.read()
+		return fulltext
 
 class LogFile:
 	"""Parses a qlikview logfile and creates an object with it's info more easliy accessible."""
@@ -44,32 +50,30 @@ class LogFile:
 
 	@staticmethod
 	def parse_logfile(textfile):
-		encoding = detect_encoding(textfile)
+		fulltext = decode_with_detected(textfile)
 		parsed = []
-		with open(textfile,'r',encoding=encoding) as f:
-			#Start looping through lines.
-			lines = f.readlines()
-			for l in lines:
-				#First 10 chars are the date:
-				date = l[0:10]
-				#Gap of 1 char, then next 8 are time.
-				time = l[11:19]
-				#22-26  are operation no:
-				op = l[21:25]
-				#the rest is unknown...
-				rest = l[26:].strip()
-				lp = {
-					'date':date,
-					'time':time,
-					'op':op,
-					'text':rest
-					}
-				parsed.append(lp)
+		lines = fulltext.split('\n')
+		for l in lines:
+			#First 10 chars are the date:
+			date = l[0:10]
+			#Gap of 1 char, then next 8 are time.
+			time = l[11:19]
+			#22-26  are operation no:
+			op = l[21:25]
+			#the rest is unknown...
+			rest = l[26:].strip()
+			lp = {
+				'date':date,
+				'time':time,
+				'op':op,
+				'text':rest
+				}
+			parsed.append(lp)
 		return parsed
 
-	def tag_file_lines (self):
+def tag_file_lines (self):
 		"""Tag lines that reference qvds or other files within the logfile."""
-		filesearchstring = r"(?:\s|\[|^)((?:\.\.|\\\\|\\)?[\\\w \-_]+)\.(qvd|xls[xm]?)\]?"
+		filesearchstring = r"(?:\s|\[|^)((?:\.\.|\\\\|\\)*[\\\w \-_]+)\.(qvd|xls[xm]?)\]?"
 		storesearchstring = r"store\s\[?[\w\s]*\]?\sinto"
 
 		filesearch = re.compile(filesearchstring)	#Finds a qvd. Returns the name in the capture group.
@@ -94,3 +98,22 @@ class LogFile:
 				optype = 'LOAD'
 		print('\nParsed!')
 		return matchlines
+
+	def get_files_touched(self):
+		"""returns a list of the files found by tag_file_lines"""
+		files_touched = [line['file'] for line in self.lines if 'file' in line.keys()]
+		return files_touched
+
+	def find_file(self,path):
+		"""Checks if the file exists and returns its absolute path."""
+		if os.path.isabs(path):
+			if os.path.isfile(path):
+				return (path)
+		else:
+			rootpath = os.path.dirname(self.path)
+			#print('searching in {0}'.format(rootpath))
+			joined = os.path.abspath(os.path.join(rootpath,path))
+			if os.path.isfile(joined):
+				return joined
+			else:
+				return None
