32f860635a5fa6d4a9b6937625d82222428a246c
unknown <Ben.Simonds@LPPB313.keyruscorp.key>
2016-07-26 22:33:24 +0100

    Added snippets to gitignore

--- snippets/subbify_TFL.py
+++
@@ -1,3 +0,0 @@
-from qvstools.subbify import subbify
-subbify(r'C:\Users\ben.simonds\Documents\TFL_Minimal\ETL\ETLTubeData.qvs',reload_after = 's')
-#"C:\Program Files\QlikView\qv.exe" "C:\Users\ben.simonds\Documents\TFL_Minimal\ETL\ETLTubeData.qvs" /l
